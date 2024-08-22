d5a4c7992ce62d5a0f5a6271bd1b5978eea07c52
carvalhaus <ooliveira.joaop@gmail.com>
2024-08-22 16:02:57 -0300

    style: fix style

--- frontend/app/(pages)/(homepage)/_components/slogan.jsx
+++ frontend/app/(pages)/(homepage)/_components/slogan.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 function SloganSection() {
     return (
-        <section className="w-full flex flex-col items-center justify-center text-center py-6 lg:py-20">
+        <section className="w-full flex flex-col items-center justify-center text-center py-10 lg:py-20">
             <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl w-auto md:w-1/2 lg:w-2/5">Controle suas Finanças com Facilidade</h1>
             <p className="leading-7 [&:not(:first-child)]:mt-6 w-auto md:w-1/2 lg:w-1/4">Organize suas finanças, monitore gastos, e alcance objetivos financeiros com facilidade e praticidade.</p>
-            <Button className="[&:not(:first-child)]:mt-6 h-14 w-52 text-xl"><Link href="#">Comece aqui</Link></Button>
+            <Button size="xl" className="[&:not(:first-child)]:mt-6"><Link href="#">Comece aqui</Link></Button>
         </section >
     );
 }
--- frontend/app/(pages)/(homepage)/_components/summary.jsx
+++ frontend/app/(pages)/(homepage)/_components/summary.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function SummarySection() {
     return (
-        <section className="w-full flex flex-col text-center lg:text-left lg:flex-row items-center justify-center py-6 lg:py-20 gap-8 lg:gap-32">
+        <section className="w-full flex flex-col text-center lg:text-left lg:flex-row items-center justify-center py-10 lg:py-20 gap-8 lg:gap-32">
             <div className="max-w-[480px]">
                 <h2 className="pb-2 text-3xl font-semibold tracking-tight first:mt-0">Resumo Financeiro Automatizado</h2>
                 <p className="leading-7 [&:not(:first-child)]:mt-2">Proporciona ao usuário uma visão clara e instantânea do saldo atual e das transações recentes, facilitando o acompanhamento das finanças através de limites previamente estabelecidos, com a interface fluída e acessível em toda sua utilização.</p>
