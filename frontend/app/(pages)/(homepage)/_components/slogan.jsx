7fae843521d9c6a3373444102fade6be81a2ae68
carvalhaus <ooliveira.joaop@gmail.com>
2024-08-22 15:11:02 -0300

    style: change breakpoint to responsive style

--- frontend/app/(pages)/(homepage)/_components/slogan.jsx
+++ frontend/app/(pages)/(homepage)/_components/slogan.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function SloganSection() {
     return (
-        <section className="w-full flex flex-col items-center justify-center text-center py-6 md:py-20">
+        <section className="w-full flex flex-col items-center justify-center text-center py-6 lg:py-20">
             <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl w-auto md:w-1/2 lg:w-2/5">Controle suas Finanças com Facilidade</h1>
             <p className="leading-7 [&:not(:first-child)]:mt-6 w-auto md:w-1/2 lg:w-1/4">Organize suas finanças, monitore gastos, e alcance objetivos financeiros com facilidade e praticidade.</p>
             <Button className="[&:not(:first-child)]:mt-6 h-14 w-52 text-xl"><Link href="#">Comece aqui</Link></Button>
