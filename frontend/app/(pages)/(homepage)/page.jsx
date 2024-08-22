73d7a5d5bbc8d6208608e5517c95e7b74f016569
carvalhaus <ooliveira.joaop@gmail.com>
2024-08-22 15:10:35 -0300

    style: change padding

--- frontend/app/(pages)/(homepage)/page.jsx
+++ frontend/app/(pages)/(homepage)/page.jsx
@@ -1,9 +1,11 @@
 import SloganSection from "./_components/slogan";
+import SummarySection from "./_components/summary";
 
 function Homepage() {
     return (
-        <main className="flex-1 p-4">
+        <main className="flex-1 px-5">
             <SloganSection />
+            <SummarySection />
         </main>
     );
 }
