9fc5b9209886edef2be03a4dce268be9c2a2631a
carvalhaus <ooliveira.joaop@gmail.com>
2024-08-25 10:16:34 -0300

    feat: create modal FriendInvitation

---
+++ frontend/app/dashboard/_components/friendInvitation.jsx
@@ -0,0 +1,81 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const invitationFormSchema = z.object({
+    email: z.string().email({ message: "Esse campo é obrigatório" }),
+    message: z.string().min(10, { message: "Esse campo é obrigatório" }),
+})
+
+function FriendInvitation() {
+    const invitationForm = useForm({
+        resolver: zodResolver(invitationFormSchema),
+        defaultValues: {
+            email: "",
+            message: ""
+        }
+    })
+
+    function onSubmit(values) {
+        console.log(values)
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="w-full text-base" size="lg">Convide um amigo</Button>
+            </DialogTrigger>
+
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Convidar um amigo</DialogTitle>
+                    <DialogDescription>
+                        Convide um amigo para ter a mesma experiência que você utilizando o <span className="italic font-medium">Treasr</span>!
+                    </DialogDescription>
+                </DialogHeader>
+
+                <Form {...invitationForm}>
+                    <form onSubmit={invitationForm.handleSubmit(onSubmit)} className="flex flex-col gap-4">
+
+
+                        <FormField control={invitationForm.control} name="email" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-base">Email</FormLabel>
+                                <FormControl>
+                                    <Input type="email" placeholder="exemplo@email.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+
+
+                        <FormField control={invitationForm.control} name="message" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-base">Mensagem</FormLabel>
+                                <FormControl>
+                                    <Textarea placeholder="Digite uma mensagem para seu amigo aqui." {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                        />
+
+                        <DialogClose asChild>
+                            <Button className="w-full text-lg">Enviar convite</Button>
+                        </DialogClose>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default FriendInvitation;
--- frontend/app/dashboard/layout.jsx
+++ frontend/app/dashboard/layout.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import UserInfo from "./_components/userInfo";
 import NavBar from "./_components/navBar";
-import { Button } from "@/components/ui/button";
+import FriendInvitation from "./_components/friendInvitation";
 
 function DashboardLayout({ children }) {
     return (
         <div className="flex w-svw h-svh">
-            <aside className="w-56 flex flex-col items-center py-5 px-4 gap-10">
+            <aside className="w-56 flex flex-col items-center py-5 px-4 gap-10 border-r border-r-[#E2E8F0]">
                 <Link href="/">
                     <Image src="/Logo.svg" width={164} height={46} alt="Treasure logo" priority={true} />
                 </Link>
@@ -16,7 +16,7 @@ function DashboardLayout({ children }) {
 
                 <NavBar />
 
-                <Button>Convide um amigo</Button>
+                <FriendInvitation />
             </aside>
 
             <div className="flex-1 bg-red-50">
