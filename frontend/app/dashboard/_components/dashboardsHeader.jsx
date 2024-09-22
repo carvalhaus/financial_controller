99e27d663bdfeb425383dadeb385af21bfdf9951
carvalhaus <ooliveira.joaop@gmail.com>
2024-09-22 16:19:03 -0300

    feat: add Toast

--- frontend/app/dashboard/_components/dashboardsHeader.jsx
+++ frontend/app/dashboard/_components/dashboardsHeader.jsx
@@ -1,14 +1,26 @@
-function DashboardsHeader({ title, subtitle }) {
-    return (
-        <header className="border-b border-b-[#E2E8F0] px-10 py-5">
-            <h1 className="scroll-m-20 text-3xl font-extrabold tracking-wide">
-                {title}
-            </h1>
-            <p className="leading-7 [&:not(:first-child)]:mt-2">
-                {subtitle}
+function DashboardsHeader({ title, category, subtitle }) {
+  return (
+    <header className="border-b border-b-[#E2E8F0] px-10 py-5">
+      <div className="flex gap-2">
+        <h1
+          className={`scroll-m-20 text-3xl font-extrabold tracking-wide ${
+            category && "opacity-80"
+          }`}
+        >
+          {title}
+        </h1>
+        {category && (
+          <>
+            <p className="text-2xl">&#8594;</p>
+            <p className="scroll-m-20 text-3xl font-extrabold tracking-wide">
+              {category}
             </p>
-        </header>
-    );
+          </>
+        )}
+      </div>
+      <p className="leading-7 [&:not(:first-child)]:mt-2">{subtitle}</p>
+    </header>
+  );
 }
 
 export default DashboardsHeader;
