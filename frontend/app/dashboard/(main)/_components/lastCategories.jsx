7dd54ce3b1feba84bb1fbeb56edc8868255bc0c1
carvalhaus <ooliveira.joaop@gmail.com>
2024-09-22 14:10:23 -0300

    feat: create file categories for const

--- frontend/app/dashboard/(main)/_components/lastCategories.jsx
+++ frontend/app/dashboard/(main)/_components/lastCategories.jsx
@@ -1,71 +1,6 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import CategoryCard from "../../_components/categoryCard";
-
-const categories = [
-  {
-    title: "Alimentação",
-    quantity: 15,
-    limit: 500,
-    spent: 420,
-    remaining: 80,
-  },
-  {
-    title: "Transporte",
-    quantity: 10,
-    limit: 300,
-    spent: 250,
-    remaining: 50,
-  },
-  {
-    title: "Lazer",
-    quantity: 5,
-    limit: 200,
-    spent: 120,
-    remaining: 80,
-  },
-  {
-    title: "Moradia",
-    quantity: 1,
-    limit: 1200,
-    spent: 1000,
-    remaining: 200,
-  },
-  {
-    title: "Saúde",
-    quantity: 3,
-    limit: 400,
-    spent: 300,
-    remaining: 100,
-  },
-  {
-    title: "Vestuário",
-    quantity: 8,
-    limit: 350,
-    spent: 280,
-    remaining: 70,
-  },
-  {
-    title: "Educação",
-    quantity: 2,
-    limit: 800,
-    spent: 600,
-    remaining: 200,
-  },
-  {
-    title: "Beleza",
-    quantity: 4,
-    limit: 150,
-    spent: 100,
-    remaining: 50,
-  },
-  {
-    title: "Outras",
-    quantity: 7,
-    limit: 250,
-    spent: 180,
-    remaining: 70,
-  },
-];
+import { categories } from "@/lib/categories";
 
 function LastCategories() {
   return (
@@ -79,6 +14,7 @@ function LastCategories() {
           {categories.map((category, index) => (
             <CategoryCard
               key={index}
+              emoji={category.emoji}
               title={category.title}
               quantity={category.quantity}
               limit={category.limit}
