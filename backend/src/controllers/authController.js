fd9b1fb526e7938db5cafbfdc0a8b10665c2c938
carvalhaus <ooliveira.joaop@gmail.com>
2024-10-05 09:43:39 -0300

    feat: change files name and add googleOAuthHandler

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,99 @@
-const googleOAuthHandler = async (req, res) => {
-  //24:39
+const { userService, googleOAuthService } = require("../services/authService");
+const hashPassword = require("../utils/hashPassword");
+const createJwt = require("../utils/createJwt");
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
+
+const authController = {
+  register: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const hashedPassword = await hashPassword(password);
+
+      const newUser = await userService.registerUser(email, hashedPassword);
+
+      const token = createJwt(newUser);
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 3600000,
+        sameSite: "strict",
+      });
+
+      res.status(201).json({
+        message: "Usuário registrado com sucesso!",
+        user: { email: newUser.email },
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const user = await userService.loginUser(email);
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ error: "E-mail ou senha inválidos!" });
+      }
+
+      const token = createJwt(user);
+
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: false,
+        maxAge: 3600000,
+        sameSite: "strict",
+      });
+
+      res.status(200).json({
+        message: "Login bem-sucedido!",
+        user: { email: user.email },
+        token,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 
-module.exports = {
-  googleOAuthHandler,
+const googleOAuthHandler = async (req, res) => {
+  const code = req.query.code;
+
+  try {
+    const { id_token, access_token } =
+      await googleOAuthService.getGoogleOAuthTokens({ code });
+
+    const googleUser = await googleOAuthService.getGoogleUser({
+      id_token,
+      access_token,
+    });
+
+    const { email, name } = googleUser;
+
+    const user = await googleOAuthService.upsertGoogleUser(email, name);
+
+    const token = createJwt(user);
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 3600000,
+      sameSite: "strict",
+    });
+
+    res.redirect(`${process.env.FRONTEND_URL}/dashboard`);
+  } catch (error) {
+    console.error("OAuth Handler Error:", error.message);
+    res.status(500).json({ error: "Failed to fetch tokens" });
+    return res.redirect(process.env.FRONTEND_URL);
+  }
 };
+
+module.exports = { authController, googleOAuthHandler };
