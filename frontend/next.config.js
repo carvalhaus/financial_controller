2dfc3efd243861b8e30d3a88835f6e89afe146e5
carvalhaus <ooliveira.joaop@gmail.com>
2024-10-20 20:09:02 -0300

    Vercel file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  },
   async headers() {
     return [
       {
