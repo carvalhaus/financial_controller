1d4abeba90c9d9b27e0735491f8b04056f93af50
carvalhaus <ooliveira.joaop@gmail.com>
2024-10-20 19:37:46 -0300

    testing CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,8 @@ app.use(
   cors({
     origin: "https://treasurefinances.vercel.app",
     credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["X-CSRF-Token", "X-Requested-With", "Content-Type"],
   })
 );
 
