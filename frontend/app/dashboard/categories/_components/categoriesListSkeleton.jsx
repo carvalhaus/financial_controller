802900868d5770a64a4f9b66930b5a80bb03fd3b
carvalhaus <ooliveira.joaop@gmail.com>
2024-10-27 18:45:44 -0300

    fix deploy error

--- frontend/app/dashboard/categories/_components/categoriesListSkeleton.jsx
+++ frontend/app/dashboard/categories/_components/categoriesListSkeleton.jsx
@@ -6,7 +6,7 @@ function CategoriesListSkeleton() {
     <div className="flex-1 p-4 w-full overflow-y-scroll">
       <div className="flex flex-col-reverse justify-center md:flex-row-reverse md:flex-wrap-reverse gap-4 2xl:gap-5 w-full">
         {skeletons?.map((index) => (
-          <CategoryCardSkeleton />
+          <CategoryCardSkeleton key={index} />
         ))}
       </div>
     </div>
